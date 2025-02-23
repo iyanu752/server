5a50325b240ac710b47bb0b717264b7f68ec6db3
Ahmed iyanuoluwa <127423641+iyanu752@users.noreply.github.com>
2025-02-23 19:53:16 +0100

    'emailtemplate'

--- config/email.js
+++ config/email.js
@@ -17,25 +17,25 @@ const Email = (options) => {
 
 const sendEmail = ({ name, email, message}) => {
   const options = {
-    from: `Speedtype⚜️ <iyan752@gmail.com>`,
+    from: `Speedtype⚜️`,
     to: 'iyanu752@gmail.com',
     subject: 'Message from Speedtype',
     html: `
-        <div style="width: 100%; background-color: #f3f9ff; padding: 5rem 0">
-            <div style="max-width: 700px; background-color: #feffe1; margin: 0 auto; background-image: url('https://res.cloudinary.com/dlxrzazn6/image/upload/v1725041898/jepdinustkmgor5cltdp.png'); background-repeat: no-repeat; background-size: 50%; background-position: 130% 300%;">
-              <div style="width: 100%; background-color: #2b2b2b; padding: 20px 0">
-              <a href="https://www.darcia.agency" >
+   <div style="width: 100%; background-color: #333346; padding: 5rem 0">
+            <div style="max-width: 700px; background-color: #71c3f7; margin: 0 auto;">
+              <div style="width: 100%; background-color: #FFFFFF; padding: 20px 0">
+              <a href="https://speedtype-five.vercel.app/" >
                 <img
-                  src="https://res.cloudinary.com/dlxrzazn6/image/upload/v1725043148/mmciecekert56v0xc3y9.png"
+                  src="https://res.cloudinary.com/dlxrzazn6/image/upload/v1740336148/speedlogo_hfmhwo.svg"
                   style="width: 100%; height: 70px; object-fit: contain"
                 />
               </a> 
               </div>
-              <div style="width: 100%; gap: 10px; padding: 30px 0; display: grid">
+              <div style="width: 100%; gap: 10px; color: #333346;text padding: 30px 0; display: grid">
                 <p style="font-weight: 800; font-size: 1.2rem; padding: 0 30px">
-                  Darcia Hiring Agency
+                  Speedtype
                 </p>
-                <div style="font-size: .8rem; margin: 0 30px">
+                <div style="font-size: 1rem;  margin: 0 30px">
                     <p>Full Name: <b>${name}</b></p>
                     <p>Email: <b>${email}</b></p>
                     <p>Message: <b>${message}</b></p>
@@ -43,7 +43,7 @@ const sendEmail = ({ name, email, message}) => {
               </div>
             </div>
         </div>
-      `,
+      `
   };
 
   Email(options)
