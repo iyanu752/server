34203e6526c92c36a5c542139158b40ff6b142a5
Ahmed iyanuoluwa <127423641+iyanu752@users.noreply.github.com>
2025-02-26 12:17:18 +0100

    'mongoatlas'

--- config/db.js
+++ config/db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config()
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/users", {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,    
     });
     console.log("Connected to MongoDB");
   } catch (err) {
--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -10,7 +10,8 @@ const { authMiddleware } = require("../middleware/authMiddleware");
 const router = new express.Router();
 
 router.post("/image", authMiddleware, upload.single("userImage"), uploadImg);
-router.get("/getimage", authMiddleware, getImage);
+router.get("/getimage", authMiddleware, getImage); 
 router.get("/getprofile", authMiddleware, getUser);
 router.delete("/image", authMiddleware,  deleteImage);
 module.exports = router;
+ 
